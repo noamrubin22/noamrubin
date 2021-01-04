9fe924e5df2b7851244b7a0ee6c9585fb1f87966
Noam <noamru22@gmail.com>
2021-01-04 18:53:30 +0100

    another try for mobile

--- scripts/settings.js
+++ scripts/settings.js
@@ -120,7 +120,7 @@ function Settings() {
 
     function getCurrentImage() {
         const storedImage = localStorage.getItem("display__props-background-image");
-        return storedImage;
+        // return storedImage;
         return typeof storedImage !== "undefined" ? JSON.parse(storedImage) : { selectedImage: "none" }
     }
 
