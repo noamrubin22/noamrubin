10be148c94e06d84f84f5197e98066bbd79a34a4
Noam <noamru22@gmail.com>
2021-01-19 20:08:31 +0100

    changed styling settings background-img-pc for both mobile and desktop

--- scripts/settings.js
+++ scripts/settings.js
@@ -10,7 +10,10 @@ function Settings() {
     const desktopIconsText = document.getElementsByClassName("desktop-icon__text")[1];
     let currentTheme = document.getElementsByTagName("html")[0].className;
     let currentImage;
+    let selectedImage;
 
+    console.log(images);
+    console.log(document.getElementById("djamillia"));
     /* open settings when clicked on menu */
     clickSettingsMenu.onclick = () => {
         settingsWindow.hidden = false;
@@ -27,6 +30,7 @@ function Settings() {
     chooseTheme();
     previewTheme(getCurrentTheme());
     chooseImage();
+    getCurrentImage();
     previewImage(getCurrentImage());
 
     /* set a given theme/color-scheme */
@@ -72,9 +76,10 @@ function Settings() {
     /* sets the selected image as background */
     function previewImage({ selectedImage, title, artist }) {
         const artistInsta = document.querySelector(".link__instagram");
-        const classes = ["vika", "djamillia", "kathi", "chris", "shruti", "yossi1", "yossi2"];
+        const classes = ["vika", "djamillia", "kathi", "chris", "shruti", "yossi1", "yossi2", "since95"];
 
         if (artist) {
+            console.log(artist);
             artistInsta.hidden = false;
             switch (artist) {
                 case "kathi":
@@ -82,51 +87,80 @@ function Settings() {
                     artistInsta.href = "https://www.instagram.com/katharina.michalsky";
                     chosenBackgroundImage.classList.remove(...classes);
                     chosenBackgroundImage.classList.add(artist);
+                    backgroundImagePc.classList.remove(...classes);
+                    backgroundImagePc.classList.add(artist);
                     break;
                 case "chris":
                     artistInsta.innerHTML = "artwork by @iti.art";
                     artistInsta.href = "https://www.instagram.com/iti.art";
                     chosenBackgroundImage.classList.remove(...classes);
                     chosenBackgroundImage.classList.add(artist);
+                    backgroundImagePc.classList.remove(...classes);
+                    backgroundImagePc.classList.add(artist);
                     break;
                 case "shruti":
                     artistInsta.innerHTML = "artwork by @shrooodi";
                     artistInsta.href = "https://www.instagram.com/shrooodi";
                     chosenBackgroundImage.classList.remove(...classes);
                     chosenBackgroundImage.classList.add(artist);
+                    backgroundImagePc.classList.remove(...classes);
+                    backgroundImagePc.classList.add(artist);
                     break;
                 case "djamillia":
                     artistInsta.innerHTML = "artwork by @manush420";
                     artistInsta.href = "https://www.instagram.com/manush420"
                     chosenBackgroundImage.classList.remove(...classes);
                     chosenBackgroundImage.classList.add(artist);
+                    backgroundImagePc.classList.remove(...classes);
+                    backgroundImagePc.classList.add(artist);
                     break;
                 case "vika":
                     artistInsta.innerHTML = "artwork by @uuuuuvika";
                     artistInsta.href = "https://www.instagram.com/uuuuuvika";
                     chosenBackgroundImage.classList.remove(...classes);
                     chosenBackgroundImage.classList.add(artist);
+                    backgroundImagePc.classList.remove(...classes);
+                    backgroundImagePc.classList.add(artist);
                     break;
                 case "yossi1":
                     artistInsta.innerHTML = "artwork by @yoshikame_";
                     artistInsta.href = "https://www.instagram.com/yoshikame_";
                     chosenBackgroundImage.classList.remove(...classes);
                     chosenBackgroundImage.classList.add(artist);
+                    backgroundImagePc.classList.remove(...classes);
+                    backgroundImagePc.classList.add(artist);
                     break;
                 case "yossi2":
                     artistInsta.innerHTML = "artwork by @yoshikame_";
                     artistInsta.href = "https://www.instagram.com/yoshikame_";
                     chosenBackgroundImage.classList.remove(...classes);
                     chosenBackgroundImage.classList.add(artist);
+                    backgroundImagePc.classList.remove(...classes);
+                    backgroundImagePc.classList.add(artist);
+                    break
+                case "since95":
+                    artistInsta.hidden = true;
+                    chosenBackgroundImage.classList.remove(...classes);
+                    chosenBackgroundImage.classList.add(artist);
+                    backgroundImagePc.classList.remove(...classes);
+                    backgroundImagePc.classList.add(artist);
                     break
             }
         } else {
-            artistInsta.hidden = true;
-            console.log("nintyess");
-            chosenBackgroundImage.classList.add("since95");
-            chosenBackgroundImage.classList.remove(...classes);
+            console.log("no");
         }
 
+        // props 
+        // artistText
+        // artistInstaLink
+        // hasInstagram = true or false
+
+        // spefifyArtworkStyling() shuuld 
+        // remove all classnames
+        // add artist name as classname
+        // add text and link
+
+        // previewBackground() 
         // add image to both backgrounds (pc and real display)
         chosenBackgroundImage.src = selectedImage;
         chosenBackgroundImage.alt = title;
@@ -135,6 +169,114 @@ function Settings() {
         backgroundImagePc.src = selectedImage;
         backgroundImagePc.alt = title;
     }
+
+
+    const kathiProps = {
+        artist: "kathi",
+        artistText: "artwork by @katharina.michalsky",
+        artistInstaLink: "https://www.instagram.com/katharina.michalsky",
+        hasInstagram: true,
+        title: "Landscape",
+    }
+
+    const yossiDesktopProps = {
+        artist: "yossi1",
+        artistText: "artwork by @yoshikame_",
+        artistInstaLink: "https://www.instagram.com/yoshikame_",
+        hasInstagram: true,
+        title: "Pasha",
+    }
+
+    const yossiMobileProps = {
+        artist: "yossi2",
+        artistText: "artwork by @yoshikame_",
+        artistInstaLink: "https://www.instagram.com/yoshikame_",
+        hasInstagram: true,
+        title: "Pasha",
+    }
+
+    const djamilliaProps = {
+        artist: "djamillia",
+        artistText: "artwork by @manush420",
+        artistInstaLink: "https://www.instagram.com/manush420",
+        hasInstagram: true,
+        title: "Ich Gebäre dich",
+    }
+
+    const shrutiProps = {
+        artist: "shruti",
+        artistText: "artwork by @shrooodi",
+        artistInstaLink: "https://www.instagram.com/shrooodi",
+        hasInstagram: true,
+        title: "High_Rise",
+    }
+
+    const chrisProps = {
+        artist: "chris",
+        artistText: "artwork by @iti.art",
+        artistInstaLink: "https://www.instagram.com/iti.art",
+        hasInstagram: true,
+        title: "Inner Floating",
+    }
+
+    const vikaProps = {
+        artist: "vika",
+        artistText: "artwork by @uuuuuvika",
+        artistInstaLink: "https://www.instagram.com/uuuuuvika",
+        hasInstagram: true,
+        title: "Peaceful Warrior",
+    }
+
+    const since95Props = {
+        artist: "since95",
+        hasInstagram: false,
+        title: "90iesKid",
+    }
+
+    class Artwork {
+        constructor(props) {
+            this.props = props;
+            this.specifyStyling();
+            this.previewArtwork();
+        }
+
+        specifyStyling() {
+            const instagramLink = document.querySelector(".link__instagram");
+            const chosenBackgroundImage = document.querySelector(".monitor__background-img");
+            const artists = ["vika", "djamillia", "kathi", "chris", "shruti", "yossi1", "yossi2", "since95"];
+
+            chosenBackgroundImage.classList.remove(...artists);
+            chosenBackgroundImage.classList.add(this.props.artist);
+
+            if (this.props.hasInstagram) {
+                instagramLink.hidden = false;
+                instagramLink.innerHTML = this.props.artistText;
+                instagramLink.href = this.props.artistInstaLink;
+            } else {
+                instagramLink.hidden = true;
+            }
+        }
+
+        previewArtwork() {
+            const backgroundImagePc = document.getElementsByClassName("pc__background-img")[0];
+            chosenBackgroundImage.src = selectedImage;
+            chosenBackgroundImage.alt = this.title;
+            chosenBackgroundImage.id = this.artist;
+            backgroundImagePc.hidden = false;
+            backgroundImagePc.src = selectedImage;
+            backgroundImagePc.alt = this.title;
+        }
+    }
+
+
+    // new Artwork(kathiProps);
+    // new Artwork(djamilliaProps);
+    // new Artwork(yossiDesktopProps);
+    // new Artwork(yossiMobileProps);
+    // new Artwork(shrutiProps);
+    // new Artwork(chrisProps);
+    // new Artwork(since95Props);
+    // new Artwork(vikaProps);
 
     function getCurrentImage() {
         const storedImage = localStorage.getItem("display__props-background-image");
